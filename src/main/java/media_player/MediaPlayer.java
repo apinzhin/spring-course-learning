d41e14104f792d896368ac8d3eff496f8eb6e5a4
apinzhin <lightF0rce>
2018-11-07 16:35:15 +0700

    spring task 1: how to get bean(s) from context

--- src/main/java/media_player/MediaPlayer.java
+++ src/main/java/media_player/MediaPlayer.java
@@ -9,7 +9,16 @@ public class MediaPlayer {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
+        System.out.println("--------------- Get bean by Id ");
         MediaDevice device = context.getBean("diskDevice", MediaDevice.class);
         device.play();
+
+        System.out.println("--------------- Get bean by Class ");
+        FlashDevice flashDevice = context.getBean(FlashDevice.class);
+        flashDevice.play();
+
+        System.out.println("--------------- Get beans by Interface ");
+        Map<String, MediaDevice> devices= context.getBeansOfType(MediaDevice.class);
+        devices.values().forEach(MediaDevice::play);
     }
 }
