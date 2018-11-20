5620780b19e3082a4e394a80ea758cbdc94f9c78
Alexey Pinzhin <apinzhin@promedico.nl>
2018-11-20 14:31:51 +0700

    UnitTesting: part 2 - test config

--- src/main/java/media_player/MockConnector.java
+++ src/main/java/media_player/MockConnector.java
@@ -1,22 +1,33 @@
 package media_player;
 
+import com.google.common.collect.Lists;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Component("connector")
 @Profile("test")
 public class MockConnector extends OnlineChannelConnector {
+
+
+    private ArrayList<String> channels;
+
+    @Value("${channels}")
+    public void setChannels(String[] channelsArr) {
+        channels = Lists.newArrayList(channelsArr);
+    }
 
     @PostConstruct
     @SneakyThrows
     public void init() {
         System.out.println("Mock channel....");
 
-        OnlineChannels.channels = Arrays.asList("Radio Test 1", "Radio Test 2");
+        OnlineChannels.channels = channels.stream().map(e-> "Open channel: " + e).collect(Collectors.toList());
     }
 
 }
--- src/test/java/MediaPlyerTest.java
+++ src/test/java/MediaPlyerTest.java
@@ -1,9 +1,11 @@
 import media_player.CdDevice;
-import media_player.MediaPlayerConfig;
+import media_player.Connector;
+import media_player.OnlineChannels;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,11 +18,15 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles({"research", "test"})
-@ContextConfiguration(classes = MediaPlayerConfig.class)
+@ContextConfiguration(classes = TestMediaPlayerConfig.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MediaPlyerTest {
 
     @Autowired
     private CdDevice cdDevice;
+
+    @Autowired
+    Connector connector;
 
     @BeforeClass
     public static void before() {
@@ -30,6 +36,11 @@ public class MediaPlyerTest {
     @Test
     public void testShuffle() {
         assertEquals(cdDevice.getSongsOrdered().size(), cdDevice.getDisk().getSongs().size());
+    }
+
+    @Test
+    public void testConnector() {
+        assertEquals(OnlineChannels.channels.get(0), "Open channel: Radio Test");
     }
 
 }
